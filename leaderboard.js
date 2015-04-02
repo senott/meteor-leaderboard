ded1b89d0a8251aa9850b911d8b9b0d64f3b1d5f
Senott <senott@yahoo.com>
2015-04-02 11:44:56 -0300

    Chapter 9

--- leaderboard.js
+++ leaderboard.js
@@ -3,7 +3,7 @@ PlayersList = new Mongo.Collection('players');
 if(Meteor.isClient){
 	Template.leaderboard.helpers({
 		'player': function(){
-			return PlayersList.find()
+			return PlayersList.find({}, { sort: { score: -1, name: 1 } })
 		},
 		'players': function(){
 			return PlayersList.find().count()
@@ -14,12 +14,29 @@ if(Meteor.isClient){
 			if(playerId == selectedPlayer) {
 				return 'selected'
 			}
+		},
+		'showSelectedPlayer': function() {
+			var selectedPlayer = Session.get('selectedPlayer');
+			return PlayersList.findOne(selectedPlayer)
 		}
 	});
 	Template.leaderboard.events({
 		'click .player': function() {
 			var playerId = this._id;
 			Session.set('selectedPlayer', playerId);
+		},
+		'click .increment': function() {
+			var selectedPlayer = Session.get('selectedPlayer');
+			PlayersList.update(selectedPlayer, { $inc: { score: 5 } });
+		},
+		'click .decrement': function() {
+			var selectedPlayer = Session.get('selectedPlayer');
+			PlayersList.update(selectedPlayer, { $inc: { score: -5 } });
+		}
+	});
+	Template.addPlayerForm.events({
+		'submit form': function() {
+			console.log("Form submitted");
 		}
 	});
 }
