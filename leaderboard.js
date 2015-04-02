e44d254ecbb5575d73f8ad02e67a4b8d94261d47
Senott <senott@yahoo.com>
2015-04-01 23:45:40 -0300

    first commit

---
+++ leaderboard.js
@@ -0,0 +1,25 @@
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+	Template.leaderboard.helpers({
+		'player': function(){
+			return PlayersList.find()
+		},
+		'players': function(){
+			return PlayersList.find().count()
+		},
+		'selectedClass': function(){
+			var playerId = this._id;
+			var selectedPlayer = Session.get('selectedPlayer');
+			if(playerId == selectedPlayer) {
+				return 'selected'
+			}
+		}
+	});
+	Template.leaderboard.events({
+		'click .player': function() {
+			var playerId = this._id;
+			Session.set('selectedPlayer', playerId);
+		}
+	});
+}
