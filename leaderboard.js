fc851fc1d5fbd73a749a6d7c8772c0b7e27dc415
Senott <senott@yahoo.com>
2015-04-13 00:15:05 -0300

    End of chapter 12

--- leaderboard.js
+++ leaderboard.js
@@ -32,15 +32,15 @@ if(Meteor.isClient){
 		},
 		'click .increment': function() {
 			var selectedPlayer = Session.get('selectedPlayer');
-			PlayersList.update(selectedPlayer, { $inc: { score: 5 } });
+			Meteor.call('modifyPlayerScore', selectedPlayer, 5);
 		},
 		'click .decrement': function() {
 			var selectedPlayer = Session.get('selectedPlayer');
-			PlayersList.update(selectedPlayer, { $inc: { score: -5 } });
+			Meteor.call('modifyPlayerScore', selectedPlayer, -5);
 		},
 		'click .remove': function() {
 			var selectedPlayer = Session.get('selectedPlayer');
-			PlayersList.remove(selectedPlayer);
+			Meteor.call('removePlayerData', selectedPlayer);
 		}
 	});
 	
@@ -48,18 +48,29 @@ if(Meteor.isClient){
 		'submit form': function(event) {
 			event.preventDefault();
 			var playerNameVar = event.target.playerName.value;
-			var currentUserId = Meteor.userId();
-			PlayersList.insert({
-				name: playerNameVar,
-				score: 0,
-				createdBy: currentUserId
-			});
+			Meteor.call('insertPlayerData', playerNameVar);
 			event.target.playerName.value = '';
 		}
 	});
 }
 
 if(Meteor.isServer){
+	Meteor.methods({
+		'insertPlayerData': function(playerNameVar) {
+			var currentUserId = Meteor.userId();
+			PlayersList.insert({
+				name: playerNameVar,
+				score: 0,
+				createdBy: currentUserId
+			});
+		},
+		'removePlayerData': function(selectedPlayer) {
+			PlayersList.remove(selectedPlayer);
+		},
+		'modifyPlayerScore': function(selectedPlayer, scoreValue) {
+			PlayersList.update( selectedPlayer, { $inc: { score: scoreValue } } );
+		}
+	});
 	Meteor.publish('thePlayers', function(){
 		var currentUserId = this.userId;
 		return PlayersList.find( { createdBy: currentUserId } )
